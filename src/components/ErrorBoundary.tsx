84f108f7f969d5c5c3280ec910f7323b51df2f46
Ismail Khair <frontend.dev.ish@gmail.com>
2025-09-03 16:29:49 -0400

    fix: lint err

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -1,4 +1,5 @@
-import React, { Component, type ReactNode } from 'react';
+import React, { Component } from 'react';
+import type { ReactNode } from 'react';
 import { Alert, Button, Box } from '@mui/material';
 
 interface Props {
